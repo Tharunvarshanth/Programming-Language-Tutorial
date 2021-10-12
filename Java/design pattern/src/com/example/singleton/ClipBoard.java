50cccb023b8ae25c7b0cf8a4de8d400bafc7089d
Thiluxan Subramaniam <thiluxan@hotmail.co.uk>
2021-10-12 09:55:37 +0530

    Singleton Pattern example added

---
+++ Java/design pattern/src/com/example/singleton/ClipBoard.java
@@ -0,0 +1,28 @@
+package com.example.singleton;
+
+public class ClipBoard {
+
+    public static ClipBoard instance = null;
+
+    private Object data;
+
+    private ClipBoard(){
+
+    }
+
+    public static ClipBoard getInstance(){
+        if (instance == null){
+            instance = new ClipBoard();
+        }
+
+        return instance;
+    }
+
+    public void copy(Object value){
+        this.data = value;
+    }
+
+    public Object paste(){
+        return data;
+    }
+}
